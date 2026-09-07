1d8947b5ba14f743e8a0348aebbfd13a2ee7e56e
Achi00 <toolvlg@gmail.com>
2026-09-07 21:38:52 +0400

    introduced resp valued enums and RespValue struct

---
+++ Parser/Enums/RespValueType.cs
@@ -0,0 +1,12 @@
+﻿namespace InMemoryDatabase.Parser.Enums
+{
+    public enum RespValueType
+    {
+        SimpleString,
+        Error, 
+        Integer, 
+        BulkString,
+        Array, 
+        Null
+    }
+}
---
+++ Parser/Models/RespValue.cs
@@ -0,0 +1,12 @@
+﻿using InMemoryDatabase.Parser.Enums;
+
+namespace InMemoryDatabase.Parser.Models
+{
+    public struct RespValue
+    {
+        public RespValueType Type { get; }
+        public string String { get; }
+        public long Integer { get; }
+        public RespValue[] Array { get; }
+    }
+}
