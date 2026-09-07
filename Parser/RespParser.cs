80106070701b93a1c9a1cd4fed9427f0006651a2
Achi00 <toolvlg@gmail.com>
2026-09-07 21:56:16 +0400

    add zero allication recursive array parser

--- Parser/RespParser.cs
+++ Parser/RespParser.cs
@@ -1,6 +1,7 @@
 ﻿using InMemoryDatabase.Exceptions;
 using InMemoryDatabase.Parser.Models;
 using System.Buffers;
+using System.Buffers.Text;
 
 namespace InMemoryDatabase.Parser
 {
@@ -24,9 +25,53 @@ namespace InMemoryDatabase.Parser
             };
         }
 
+        // zero allication array parser
         private static RespValue ParseArray(ref SequenceReader<byte> reader)
         {
-            throw new NotImplementedException();
+            int count = ReadIntLine(ref reader);
+
+            var items = new RespValue[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                items[i] = ParseArray(ref reader);
+            }
+
+            return RespValue.Array(items);
+        }
+
+        private static int ReadIntLine(ref SequenceReader<byte> reader)
+        {
+            // look for delimiter \r\n
+            ReadOnlySpan<byte> crlf = stackalloc byte[] { (byte)'\r', (byte)'\n' };
+
+            if (!reader.TryReadTo(out ReadOnlySequence<byte> line, crlf))
+            {
+                throw new RespProtocolException("Incomplete line");
+            }
+
+            // ascii to integer
+            if (line.IsSingleSegment)
+            {
+                if (!Utf8Parser.TryParse(line.FirstSpan, out int result, out _))
+                {
+                    throw new RespProtocolException("Invalid integer format");
+                }
+                return result;
+            }
+            else
+            {
+                // fallback for multi-segment sequences
+                Span<byte> localSpan = stackalloc byte[(int)line.Length];
+                line.CopyTo(localSpan);
+               
+                if (!System.Buffers.Text.Utf8Parser.TryParse(localSpan, out int result, out _))
+                {
+                    throw new RespProtocolException("Invalid integer format");
+                }
+
+                return result;
+            }
         }
 
         private static RespValue ParseBulkString(ref SequenceReader<byte> reader)
