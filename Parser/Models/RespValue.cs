8c308f312942b1fc31a70de0a344d2e37834671b
Achi00 <toolvlg@gmail.com>
2026-09-07 21:44:21 +0400

    created new  RespValue as rest parsing value type

--- Parser/Models/RespValue.cs
+++ Parser/Models/RespValue.cs
@@ -2,11 +2,23 @@
 
 namespace InMemoryDatabase.Parser.Models
 {
-    public struct RespValue
+    public readonly struct RespValue
     {
         public RespValueType Type { get; }
-        public string String { get; }
-        public long Integer { get; }
-        public RespValue[] Array { get; }
+        public string TypeString { get; }
+        public long TypeInteger { get; }
+        public RespValue[] TypeArray { get; }
+
+        private RespValue(RespValueType type, string? str = null, long integer = 0, RespValue[]? array = null)
+        {
+            Type = type; TypeString = str; TypeInteger = integer; TypeArray = array;
+        }
+
+        public static RespValue SimpleString(string s) => new(RespValueType.SimpleString, str: s);
+        public static RespValue Error(string s) => new(RespValueType.Error, str: s);
+        public static RespValue Integer(long i) => new(RespValueType.Integer, integer: i);
+        public static RespValue BulkString(string s) => new(RespValueType.BulkString, str: s);
+        public static RespValue Array(RespValue[] items) => new(RespValueType.Array, array: items);
+        public static RespValue Null { get; } = new(RespValueType.Null);
     }
 }
