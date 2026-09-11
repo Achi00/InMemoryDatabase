8c444a8f7a92c656087a6a9a384dbba54bee0bfd
Achi00 <toolvlg@gmail.com>
2026-09-11 21:43:45 +0400

    introduce parser handler, used new RespIncompleteDataException w/o throwing

---
+++ Handlers/RespConnectionHandler.cs
@@ -0,0 +1,39 @@
+﻿using InMemoryDatabase.Exceptions;
+using InMemoryDatabase.Parser;
+using InMemoryDatabase.Parser.Models;
+using System.Buffers;
+using System.IO.Pipelines;
+
+namespace InMemoryDatabase.Handlers
+{
+    public class RespConnectionHandler
+    {
+        public async Task ProcessAsync(PipeReader reader, CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                
+            }
+        }
+
+        private static bool TryParseOne(ref ReadOnlySequence<byte> buffer, out RespValue command)
+        {
+            var reader = new SequenceReader<byte>(buffer);
+
+            try
+            {
+                command = RespParser.ParseValue(ref reader, depth: 0);
+                // consumes already seccessfully parsed data
+                buffer = buffer.Slice(reader.Position);
+
+                return true;
+            }
+            catch (RespIncompleteDataException)
+            {
+                command = default;
+                // wait for more data from pipe, buffer is unchanged, no throwing
+                return false;
+            }
+        }
+    }
+}
