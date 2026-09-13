be3d8f1d65f2a707f877f178e70a32ad9d056719
Achi00 <toolvlg@gmail.com>
2026-09-13 19:47:14 +0400

    tested multi segment flow too, simulated as incoming traffic in tcp

--- Program.cs
+++ Program.cs
@@ -41,4 +41,31 @@ TestParse("incomplete", Encoding.UTF8.GetBytes("$5\r\nhel")); // should throw Re
 TestParse("bad prefix", Encoding.UTF8.GetBytes("?xyz\r\n")); // should throw RespProtocolException
 
 
+// multi segment path testing
+// split across two segments to simulate simple incoming traffic behaivor
+byte[] part1 = Encoding.UTF8.GetBytes("*1\r\n:12");
+byte[] part2 = Encoding.UTF8.GetBytes("34\r\n");
+var first = new SequenceSegment(part1);
+var last = first.Add(part2);
+var multiSegSequence = new ReadOnlySequence<byte>(first, 0, last, part2.Length);
+
+var reader2 = new SequenceReader<byte>(multiSegSequence);
+RespValue result2 = RespParser.ParseValue(ref reader2, depth: 0);
+
+
+Console.WriteLine(result2.Type);
+Console.WriteLine(result2.TypeString);
+Console.WriteLine(result2.TypeInteger);
+Console.WriteLine(result2.TypeArray);
+class SequenceSegment : ReadOnlySequenceSegment<byte>
+{
+    public SequenceSegment(ReadOnlyMemory<byte> memory) => Memory = memory;
+    public SequenceSegment Add(ReadOnlyMemory<byte> mem)
+    {
+        var segment = new SequenceSegment(mem) { RunningIndex = RunningIndex + Memory.Length };
+        Next = segment;
+        return segment;
+    }
+}
+
 //await TCPServer.Start(CancellationToken.None);
