c56ab1ac4fd40355c5006bd9a6a69a10014bba46
Achi00 <toolvlg@gmail.com>
2026-09-13 20:22:57 +0400

    creating new tcp listener for resp

---
+++ Servers/RespServer.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace InMemoryDatabase.Servers
+{
+    internal class RespServer
+    {
+        private readonly TcpListener _listener;
+
+        public RespServer(int port)
+        {
+            _listener = new TcpListener(IPAddress.Loopback, port);
+        }
+
+        public async Task RunAsync(CancellationToken ct)
+        {
+            _listener.Start();
+            Console.WriteLine("Server listening...");
+        }
+    }
+}
