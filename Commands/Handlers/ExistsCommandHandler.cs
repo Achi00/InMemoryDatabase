f414f93cda3cb20efef6d37d6b31b4bb0d78b1aa
Achi00 <toolvlg@gmail.com>
2026-09-15 23:08:54 +0400

    added exists command

---
+++ Commands/Handlers/ExistsCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using InMemoryDatabase.Parser.Models;
+using InMemoryDatabase.Storage;
+
+namespace InMemoryDatabase.Commands.Handlers
+{
+    public class ExistsCommandHandler : ICommandHandler
+    {
+        private readonly RespStore _store;
+        public string Name => "EXISTS";
+
+        public ExistsCommandHandler(RespStore store)
+        {
+            _store = store;
+        }
+
+        public RespValue Execute(RespValue[] args)
+        {
+            if (args.Length < 2)
+            {
+                return RespValue.Error("ERR wrong number of arguments for 'exists' command");
+            }
+
+            int count = 0;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (_store.TryGet(args[i].TypeString!, out _))
+                {
+                    count++;
+                }
+            }
+
+            return RespValue.Integer(count);
+        }
+    }
+}
