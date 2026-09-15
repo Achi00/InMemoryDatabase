e65f58eeac2ae57afd73441f37dc7a72db45ac91
Achi00 <toolvlg@gmail.com>
2026-09-15 19:33:47 +0400

    create strategy pattern for commands

---
+++ Commands/Handlers/SetCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using InMemoryDatabase.Parser.Models;
+using InMemoryDatabase.Storage;
+
+namespace InMemoryDatabase.Commands.Handlers
+{
+    internal class SetCommandHandler : ICommandHandler
+    {
+        private readonly RespStore _store;
+
+        public SetCommandHandler(RespStore store)
+        {
+            _store = store;
+        }
+        public string Name => "SET";
+
+        public RespValue Execute(RespValue[] args)
+        {
+            if (args.Length != 3)
+            {
+                return RespValue.Error("ERR wrong number of arguments for 'set' command");
+            }
+
+            string key = args[1].TypeString!;
+            _store.Set(key, RespValue.BulkString(args[2].TypeString!));
+
+            return RespValue.SimpleString("OK");
+        }
+    }
+}
---
+++ Commands/ICommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using InMemoryDatabase.Parser.Models;
+
+namespace InMemoryDatabase.Commands
+{
+    public interface ICommandHandler
+    {
+        public string Name { get; }
+        RespValue Execute(RespValue[] args);
+    }
+}
