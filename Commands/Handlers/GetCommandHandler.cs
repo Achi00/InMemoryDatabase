d07182d74d10cac2cc4c6ac4c4f41db52ae8da9d
Achi00 <toolvlg@gmail.com>
2026-09-15 19:35:32 +0400

    implament add command handler

---
+++ Commands/Handlers/GetCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using InMemoryDatabase.Parser.Models;
+using InMemoryDatabase.Storage;
+
+namespace InMemoryDatabase.Commands.Handlers
+{
+    public class GetCommandHandler : ICommandHandler
+    {
+        private readonly RespStore _store;
+        public string Name => "GET";
+
+        public GetCommandHandler(RespStore store)
+        {
+            _store = store;
+        }
+
+        public RespValue Execute(RespValue[] args)
+        {
+            if (args.Length != 2)
+            {
+                return RespValue.Error("ERR wrong number of arguments for 'get' command");
+            }
+
+            string key = args[1].TypeString!;
+            return _store.TryGet(key, out RespValue value) ? value : RespValue.Null;
+        }
+    }
+}
