f46b3f4322f95ebfb41d0d2ba076fd72ba88b676
Achi00 <toolvlg@gmail.com>
2026-09-16 21:05:45 +0400

    added ttl model wich holds RespValue + DateTime

---
+++ Resp/Models/StoredEntry.cs
@@ -0,0 +1,19 @@
+﻿using InMemoryDatabase.Parser.Models;
+
+namespace InMemoryDatabase.Resp.Models
+{
+    // storage which holds respvalue + datetime for TTL adn expiration
+    public readonly struct StoredEntry
+    {
+        public RespValue Value { get; }
+        public DateTimeOffset? ExpiresAt { get; }
+
+        public StoredEntry(RespValue value, DateTimeOffset? expiresAt = null)
+        {
+            Value = value;
+            ExpiresAt = expiresAt;
+        }
+
+        public bool IsExpired => ExpiresAt.HasValue && ExpiresAt.Value <= DateTimeOffset.UtcNow;
+    }
+}
