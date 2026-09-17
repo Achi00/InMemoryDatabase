c20e67f47e188973d7745fa01bb25bc2336b73d8
Achi00 <toolvlg@gmail.com>
2026-09-17 17:59:27 +0400

    fix remove of accessed expired data in RespStore.TryGet

--- Storage/RespStore.cs
+++ Storage/RespStore.cs
@@ -14,14 +14,15 @@ namespace InMemoryDatabase.Storage
             _data[key] = new StoredEntry(value, expiresAt);
         }
 
-        // checks if expired, lazy eviction strategy
+        // checks if expired, lazy eviction strategy, only remove when convenient, reduce cpu overhead
+        // expired key value pair is removed when accessed
         public bool TryGet(string key, out RespValue value)
         {
             if (_data.TryGetValue(key, out StoredEntry entry))
             {
                 if (entry.IsExpired)
                 {
-                    _data.TryGetValue(key, out _);
+                    _data.TryRemove(key, out _);
                     value = default;
                     return false;
                 }
