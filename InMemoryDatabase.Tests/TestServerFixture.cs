4e8833d3b39c3c7eadedf0b7c5be0d7fbafc4ae3
Achi00 <toolvlg@gmail.com>
2026-09-22 22:59:26 +0400

    moved git folders in solution level

---
+++ InMemoryDatabase.Tests/TestServerFixture.cs
@@ -0,0 +1,43 @@
+﻿using InMemoryDatabase.Servers;
+using System.Net;
+using System.Net.Sockets;
+
+namespace InMemoryDatabase.Tests
+{
+    // each test should have own RespServer to avoid shared state while testing
+    public class TestServerFixture : IAsyncDisposable
+    {
+        private readonly RespServer _server;
+        private readonly CancellationTokenSource _cts = new();
+        private readonly Task _serverTask;
+
+        public int Port { get; }
+
+        public TestServerFixture()
+        {
+            Port = GetFreePort();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            _cts.Cancel();
+
+            try
+            {
+                await _serverTask;
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        }
+        private int GetFreePort()
+        {
+            // post 0 = OS should pick free one
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            int port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        }
+    }
+}
