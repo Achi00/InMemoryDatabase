1dfbaf0904ce5a3a644dbbf2c27ca1023cf4ecc3
Achi00 <toolvlg@gmail.com>
2026-09-22 23:02:51 +0400

    initialize test command handlers and executor for TestServer

--- InMemoryDatabase.Tests/TestServerFixture.cs
+++ InMemoryDatabase.Tests/TestServerFixture.cs
@@ -1,4 +1,8 @@
-﻿using InMemoryDatabase.Servers;
+﻿using InMemoryDatabase.Commands;
+using InMemoryDatabase.Commands.Handlers;
+using InMemoryDatabase.Exucutors;
+using InMemoryDatabase.Servers;
+using InMemoryDatabase.Storage;
 using System.Net;
 using System.Net.Sockets;
 
@@ -16,6 +20,27 @@ namespace InMemoryDatabase.Tests
         public TestServerFixture()
         {
             Port = GetFreePort();
+
+            var store = new RespStore();
+            var handlers = new ICommandHandler[]
+            {
+                new PingCommandHandler(),
+                new SetCommandHandler(store),
+                new GetCommandHandler(store),
+                new DelCommandHandler(store),
+                new ExistsCommandHandler(store),
+                new ExpireCommandHandler(store),
+                new TtlCommandHandler(store),
+                new IncrCommandHandler(store),
+                new DecrCommandHandler(store),
+                new IncrByCommandHandler(store),
+                new DecrByCommandHandler(store),
+            };
+
+            var executor = new RespCommandExecutor(handlers);
+
+            _server = new RespServer(Port, executor);
+            _serverTask = _server.RunAsync(_cts.Token);
         }
 
         public async ValueTask DisposeAsync()
