b4c22cb07805d3664ab603346ca0c058c1c9e15f
Achi00 <toolvlg@gmail.com>
2026-09-24 23:20:57 +0400

    added non existing key test for expiration

--- InMemoryDatabase.Tests/ExpireTests.cs
+++ InMemoryDatabase.Tests/ExpireTests.cs
@@ -32,5 +32,20 @@ namespace InMemoryDatabase.Tests
 
             Assert.Equal(":1\r\n", response);
         }
+
+        [Fact]
+        public async Task Expire_WhenKeyNotExists_ShouldReturnInteger()
+        {
+            await using var client = await RespTestClient.ConnectAsync(_fixture.Port);
+
+            // no set
+
+            // expire
+            await client.SendAsync("*3\r\n$6\r\nEXPIRE\r\n$3\r\nfoo\r\n$2\r\n60\r\n");
+
+            string response = await client.ReadRawAsync();
+
+            Assert.Equal(":0\r\n", response);
+        }
     }
 }
